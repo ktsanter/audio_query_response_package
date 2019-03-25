e3ecf3e76b73c1c0a8a523ef9acb6b199199f396
SpunkyAardvark123 <ktsanter@gmail.com>
2019-03-24 21:07:32 -0400

    added ZIP packaging

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,24 @@ const app = function () {
     streamavailable: false,
     mediarecorder: [],
     audiochunks: [],
+    mp3blobs: [],
     recordbuttonstyling: {
       'start': {buttontext: 'record', buttonclass: 'start-recording'},
       'stop': {buttontext: 'stop', buttonclass: 'stop-recording'},
       'redo': {buttontext: 'redo', buttonclass: 'redo-recording'}
     }
   };
+  
+  const config = {  // this should be made from query params
+    title: "Title of query/response package",
+    readme: "This comment should describe the package",
+    downloadfilename: "arp_package000",
+    items: [
+      { textprompt: "text prompt #1", audioprompt: null },
+      { textprompt: "text prompt #2", audioprompt: null },
+      { textprompt: "text prompt #3", audioprompt: null }
+    ]
+  }
   
 	//---------------------------------------
 	// get things going
@@ -47,7 +59,68 @@ const app = function () {
 	// page rendering
 	//-----------------------------------------------------------------------------  
   function _renderPage() {
-    _enableRecordButtons(settings.streamavailable);
+    if (settings.streamavailable) {
+      page.contents.appendChild(_renderTitle(config.title));
+      page.contents.appendChild(_renderItems(config.items));  
+      page.contents.appendChild(_createPackageControl());
+    }
+  }
+  
+  function _renderTitle(title) {
+    var elemContainer = document.createElement('div');
+    elemContainer.classList.add('arp-title');
+    elemContainer.innerHTML = title;
+    
+    return elemContainer;
+  }
+
+  function _renderItems(items) {
+    var elemContainer = document.createElement('div');
+
+    for (var i = 0; i < items.length; i++) {
+        elemContainer.appendChild(_createItem(i, items[i]));
+    }
+    
+    return elemContainer;
+  }
+  
+  function _createItem(index, item) {
+    var elemContainer = document.createElement('div');
+    elemContainer.classList.add('response-container');
+    
+    var elemPrompt = document.createElement('div');
+    elemPrompt.innerHTML = item.textprompt;
+    elemContainer.appendChild(elemPrompt);
+    
+    var elemButton = document.createElement('button');
+    elemButton.id = _numberElementId('btnRecording', index);
+    elemButton.classList.add('record-control');
+    _setRecordButtonStyling(elemButton, 'start');
+    elemButton.onclick = e => _recordButtonHandler(e.target);
+    elemContainer.appendChild(elemButton);
+    
+    var elemAudio = document.createElement('audio');
+    elemAudio.id = _numberElementId('recordedAudio', index);
+    elemAudio.classList.add('audio-control');
+    elemAudio.innerHTML = 'HTML 5 audio control not supported by this browser';
+    elemContainer.appendChild(elemAudio);
+    
+    return elemContainer;
+  }
+  
+  function _createPackageControl() {
+    var elemContainer = document.createElement('div');
+    
+    var elemButton = document.createElement('button');
+    elemButton.class = 'package-control';
+    elemButton.disabled = true;
+    elemButton.innerHTML = 'package';
+    elemButton.onclick = e => _packageButtonHandler(e.target);
+    
+    page.packagebutton = elemButton;
+    elemContainer.appendChild(elemButton);
+    
+    return elemContainer;
   }
 
   function _setRecordButtonStyling(elemTarget, stageName) {
@@ -61,23 +134,20 @@ const app = function () {
     elemTarget.innerHTML = buttonText;
     elemTarget.classList.add(buttonClass);
   }
-  
-  function _enableRecordButtons(enable) {
-    var recordingButtons = document.getElementsByClassName('record-control');
-    for (var i = 0; i < recordingButtons.length; i++) {
-      recordingButtons[i].disabled = !enable;
+    
+  function _setPackageButtonEnable() {
+    var enable = true;
+    
+    for (var i = 0; i < settings.mp3blobs.length && enable; i++) {
+      enable = (settings.mp3blobs[i] != null);
     }
+    page.packagebutton.disabled = !enable;
   }
 
 	//-----------------------------------------------------------------------------
 	// audio setup and management
 	//-----------------------------------------------------------------------------  
   function _configureAudio() {
-    var recordingButtons = document.getElementsByClassName('record-control');
-    for (var i = 0; i < recordingButtons.length; i++) {
-      recordingButtons[i].onclick = e => _recordButtonHandler(e.target);
-    }
-
     navigator.mediaDevices.getUserMedia({audio:true})
     .then((stream) => _configureAudioControls(stream))
     .catch((err) => _audioConfigureError(err))
@@ -86,13 +156,14 @@ const app = function () {
   
   function _configureAudioControls(stream) {
     settings.streamavailable = true;
-    var audioElements = document.getElementsByClassName('audio-control');
-    for (var i = 0; i < audioElements.length; i++) {
-      var elemAudio = audioElements[i];
+    
+    for (var i = 0; i < config.items.length; i++) {
+      //var elemAudio = audioElements[i];
       var thisRecorder = new MediaRecorder(stream);
       var thisChunks = [];
       settings.mediarecorder.push(thisRecorder);
       settings.audiochunks.push(thisChunks);
+      settings.mp3blobs.push(null);
       thisRecorder.ondataavailable = (function(e) {
         var j = i;
         return function(e) {_finishRecording(e, j);}
@@ -145,15 +216,29 @@ const app = function () {
         elemAudio.src = URL.createObjectURL(blob);
         elemAudio.controls=true;
         elemAudio.autoplay=false;
-        sendData(blob)
+        settings.mp3blobs[index] = blob;
+        _setPackageButtonEnable();
       }
     } catch(err) {
       _reportError('_finishRecording', err);
     }
   }
+  
+  function _packageAudioRecordings() {
+    var zip = new JSZip();
+    
+    zip.file('README.txt', config.readme + '\n');  // text must end in \n
+    
+    for (var i = 0; i < settings.mp3blobs.length; i++) {
+      var blob = settings.mp3blobs[i];
+      var blobname = _numberElementId('response', (i+1)) + '.mp3';
+      zip.file(blobname, blob);
+    }
 
-  function sendData(data) {
-    console.log('implement sendData');
+    zip.generateAsync({type:"blob"})
+    .then(function(content) {
+      saveAs(content, config.downloadfilename + ".zip");
+    });
   }
   
 	//------------------------------------------------------------------
@@ -170,6 +255,10 @@ const app = function () {
       _redoRecording(elemTarget);
     }
   }    
+  
+  function _packageButtonHandler(elemTarget) {
+    _packageAudioRecordings();
+  }
 
 	//---------------------------------------
 	// utility functions
