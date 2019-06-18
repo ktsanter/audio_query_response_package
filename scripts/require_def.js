172bbed85fa23d166365a4063578be4de7bcdb80
SpunkyAardvark123 <ktsanter@gmail.com>
2019-06-18 07:59:08 -0400

    workaround for JSzip and require

--- scripts/require_def.js
+++ scripts/require_def.js
@@ -2,7 +2,7 @@ define(function (require) {
   require('main');
   require('google_webapp_interface');
   //require('commonmarkmin');
-  require('jszip_min');
+  //require('jszip_min');
   require('markdown_alt');
   
   document.addEventListener('DOMContentLoaded', app.init());
